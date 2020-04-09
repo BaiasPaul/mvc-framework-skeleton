cbb8e031b1abbb24e50da5e609f3a39a127a1f24
Baias Paul <paul.baias@evozon.com>
2020-04-09 16:27:37 +0300

    add a parameter bag

---
+++ src/Service/ParameterBag.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Framework\Service;
+
+/**
+ * Class ParameterBag
+ * @package Framework\Service
+ */
+class ParameterBag
+{
+    /**
+     * @var array
+     */
+    protected $parameters;
+
+    /**
+     * ParameterBag constructor.
+     * @param array $parameters
+     */
+    public function __construct(array $parameters = [])
+    {
+        $this->parameters = $parameters;
+    }
+
+    /**
+    * Returns the parameters.
+    *
+    * @return array An array of parameters
+    */
+    public function all()
+    {
+        return $this->parameters;
+    }
+
+    /**
+     * Returns a parameter by name.
+     *
+     * @param string $key
+     * @param mixed $default The default value if the parameter key does not exist
+     *
+     * @return mixed
+     */
+    public function get(string $key, $default = null)
+    {
+        return \array_key_exists($key, $this->parameters) ? $this->parameters[$key] : $default;
+    }
+
+    /**
+     * Sets a parameter by name.
+     *
+     * @param string $key
+     * @param mixed $value The value
+     */
+    public function set(string $key, $value)
+    {
+        $this->parameters[$key] = $value;
+    }
+}
